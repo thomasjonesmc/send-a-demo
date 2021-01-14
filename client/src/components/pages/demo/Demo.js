051d0af782b8b8a90579c0ef271ca94f8ce4a6e4
Tom <thomasjonesmc@gmail.com>
2021-01-13 19:41:53 -0600

    Added play/pause and stops demo on page change

--- client/src/components/pages/demo/Demo.js
+++ client/src/components/pages/demo/Demo.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Button } from "components/reusable/button/Button";
 import { useDemo } from "./useDemo";
 import { Track } from "./track/Track";
@@ -9,82 +9,117 @@ import { Form, FormInput } from "components/reusable/form/Form";
 import UserContext from "context/UserContext";
 import Axios from "axios";
 import { Popup } from "components/reusable/popup/Popup";
+import * as Tone from "tone";
 
-export default function Demo({location}) {
-  
+export default function Demo({ location }) {
   const [showNewTrack, setShowNewTrack] = useState(false);
-  const { demo, error, demoLoading, tracks, tracksLoading, recorder, setTracks, setError } = useDemo(location.state);
-  const [ playing, setPlaying ] = useState(false);
+  const {
+    demo,
+    error,
+    demoLoading,
+    tracks,
+    tracksLoading,
+    recorder,
+    setTracks,
+    setError,
+  } = useDemo(location.state);
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    playing ? Tone.Transport.start() : Tone.Transport.pause();
+  }, [playing]);
 
   if (demoLoading) return <span className="center">Loading Demo... 🎸</span>;
-  if (!demo) return <div className="center">No Demo Found</div>
+  if (!demo) return <div className="center">No Demo Found</div>;
 
   return (
     <div className="demoPage">
-
       <h1 id="demoTitleHeading">{demo.title}</h1>
 
       <div className="center">
-        <Button onClick={() => setShowNewTrack(true)}>
-          New Track +
-        </Button>
+        <Button onClick={() => setShowNewTrack(true)}>New Track +</Button>
       </div>
 
-      <hr style={{margin: "15px 0px"}}/>
-     
-      {tracksLoading 
-        ? <div className="center">Tracks Loading</div> 
-        : <>
+      <hr style={{ margin: "15px 0px" }} />
+
+      {tracksLoading ? (
+        <div className="center">Tracks Loading</div>
+      ) : (
+        <>
           <div className="center">
-            <Button onClick={() => setPlaying(p => !p)}>
+            <Button onClick={() => setPlaying((p) => !p)}>
               {playing ? <FaPause /> : <FaPlay />}
             </Button>
           </div>
-          {tracks.map(t => <Track key={t._id} track={t} demo={demo} recorder={recorder} playingState={[playing, setPlaying]} tracksState={[tracks, setTracks]} />)}
-          </>
-      }
+          {tracks.map((t) => (
+            <Track
+              key={t._id}
+              track={t}
+              demo={demo}
+              recorder={recorder}
+              playingState={[playing, setPlaying]}
+              tracksState={[tracks, setTracks]}
+            />
+          ))}
+        </>
+      )}
 
-      {error && <ErrorNotice clearError={() => setError(null)}>{error}</ErrorNotice>}
+      {error && (
+        <ErrorNotice clearError={() => setError(null)}>{error}</ErrorNotice>
+      )}
 
-
-      {showNewTrack && <Popup title="New Track" onExit={() => setShowNewTrack(false)}>
-        <NewTrack demoId={demo._id} setTracks={setTracks} setShowNewTrack={setShowNewTrack} />
-      </Popup>}
-  
+      {showNewTrack && (
+        <Popup title="New Track" onExit={() => setShowNewTrack(false)}>
+          <NewTrack
+            demoId={demo._id}
+            setTracks={setTracks}
+            setShowNewTrack={setShowNewTrack}
+          />
+        </Popup>
+      )}
     </div>
   );
 }
 
-export const NewTrack = ({demoId, setTracks, setShowNewTrack}) => {
-
+export const NewTrack = ({ demoId, setTracks, setShowNewTrack }) => {
   const [trackTitle, setTrackTitle] = useState("");
-  const [ error, setError ] = useState(null);
+  const [error, setError] = useState(null);
   const { user } = useContext(UserContext);
 
   const onSubmit = () => {
     if (trackTitle === "") {
       return setError("Track title is required");
     }
-    
-    Axios.post(`/demos/${demoId}/new-track`, {trackTitle, trackAuthor: user.displayName})
-      .then(res => {
+
+    Axios.post(`/demos/${demoId}/new-track`, {
+      trackTitle,
+      trackAuthor: user.displayName,
+    })
+      .then((res) => {
         const newTrack = res.data;
-        setTracks(tracks => [...tracks, {...newTrack, player: null}]);
+        setTracks((tracks) => [...tracks, { ...newTrack, player: null }]);
       })
-      .catch(err => setError(err.message));
+      .catch((err) => setError(err.message));
 
     setShowNewTrack(false);
-  }
+  };
 
   return (
     <Form onSubmit={onSubmit}>
-      <FormInput name="newTrackTitle" label="Track Title" onChange={setTrackTitle} autoFocus/>
-    
+      <FormInput
+        name="newTrackTitle"
+        label="Track Title"
+        onChange={setTrackTitle}
+        autoFocus
+      />
+
       <div className="center">
         <Button type="submit">Create New Track</Button>
       </div>
 
-      {error && <ErrorNotice clearError={() => setError(null)}>{error}</ErrorNotice>}
+      {error && (
+        <ErrorNotice clearError={() => setError(null)}>{error}</ErrorNotice>
+      )}
     </Form>
-  )
-}
+  );
+};
--- client/src/components/pages/demo/useDemo.js
+++ client/src/components/pages/demo/useDemo.js
@@ -7,91 +7,104 @@ import Recorder from "recorderjs";
 // location state will contain the demo *if* the user clicked on the demo from the `/my-demos` page
 // otherwise location state will be undefined, if it is undefined we will need to fetch the demo
 export const useDemo = (locationState) => {
+  const [demo, setDemo] = useState(null);
+  const [tracks, setTracks] = useState([]);
+  const [error, setError] = useState(null);
+  const [demoLoading, setDemoLoading] = useState(true);
+  const [tracksLoading, setTracksLoading] = useState(true);
+  const [recorder, setRecorder] = useState(null);
+  const { demoId } = useParams();
 
-    const [ demo, setDemo ] = useState(null);
-    const [ tracks, setTracks ] = useState([]);
-    const [ error, setError ] = useState(null);
-    const [ demoLoading, setDemoLoading ] = useState(true);
-    const [ tracksLoading, setTracksLoading ] = useState(true);
-    const [ recorder, setRecorder ] = useState(null);
-    const { demoId } = useParams();
+  // on first time page load we ask the user for microphone permissions
+  useEffect(() => {
+    let stream = null;
 
-    // on first time page load we ask the user for microphone permissions
-    useEffect(() => {
-        let stream = null;
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: false })
+      .then((audioStream) => {
+        stream = audioStream;
+        const audioContext = new AudioContext();
+        const input = audioContext.createMediaStreamSource(stream);
+        setRecorder(new Recorder(input, { numChannels: 1 }));
+      })
+      .catch((err) => {
+        if (err.name === "NotAllowedError") {
+          setError("Change Microphone Permissions to Record");
+        }
+      });
 
-        navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-            .then((audioStream) => {
-                stream = audioStream;
-                const audioContext = new AudioContext();
-                const input = audioContext.createMediaStreamSource(stream);
-                setRecorder(new Recorder(input, { numChannels: 1 }));
-            })
-            .catch(err => {
-                if (err.name === "NotAllowedError") {
-                    setError("Change Microphone Permissions to Record"); 
-                }
-            });
-        
-        return () => {
-          if (stream) stream.getAudioTracks()[0].stop();
-        };
-        
-    }, []);
+    return () => {
+      if (stream) stream.getAudioTracks()[0].stop();
+      Tone.Transport.stop();
+    };
+  }, []);
 
-    // any time the demoID changes, we fetch the demo with the changed ID
-    useEffect(() => {
-    
-        (async () => {
-            
-            try {
+  // any time the demoID changes, we fetch the demo with the changed ID
+  useEffect(() => {
+    // repositioning the current time of the demo to 0 seconds (in case the user was listening to another demo)
+    Tone.Transport.seconds = 0;
+    // "cancelling" old tracks (in case the user was listening to another demo.)
+    // cancel() takes in a time, default is 0 seconds. removes all audio after time
+    Tone.Transport.cancel();
+    (async () => {
+      try {
+        setDemoLoading(true);
+        setTracksLoading(true);
 
-            
-                setDemoLoading(true);
-                setTracksLoading(true);
-                
-                let currentDemo = null;
+        let currentDemo = null;
 
-                // if the locationState did not get passed in, we need to fetch the demo
-                if (locationState === undefined) {
-                    const res = await Axios.get(`/demos/${demoId}`);
-                    currentDemo = res.data;
-                } else {
-                    currentDemo = locationState.demo;
-                }
+        // if the locationState did not get passed in, we need to fetch the demo
+        if (locationState === undefined) {
+          const res = await Axios.get(`/demos/${demoId}`);
+          currentDemo = res.data;
+        } else {
+          currentDemo = locationState.demo;
+        }
 
-                setDemo(currentDemo);
-                setDemoLoading(false);
+        setDemo(currentDemo);
+        setDemoLoading(false);
 
-                const currentTracks = await Promise.all(currentDemo.tracks.map(async (track) => {
-
-                    let player = null;
-                    if (track.trackSignedURL) {
-                        player = await new Promise((resolve, reject) => {
-                            const p = new Tone.Player(track.trackSignedURL, () => {
-                                if (p) resolve(p.toDestination());
-                                else reject(new Error(`Could not create track from track signed url ${track.trackSignedURL}`));
-                            });
-                        });
-                    }
-
-                    return { ...track, player };
-                }));
-
-                setTracks(currentTracks);
-                setTracksLoading(false);
-                setError(null);
-
-            } catch (err) {
-                setDemo(null);
-                setTracks([]);
-                setError(err.message);
-                setDemoLoading(false);
-                setTracksLoading(false);
+        const currentTracks = await Promise.all(
+          currentDemo.tracks.map(async (track) => {
+            let player = null;
+            if (track.trackSignedURL) {
+              player = await new Promise((resolve, reject) => {
+                const p = new Tone.Player(track.trackSignedURL, () => {
+                  if (p) resolve(p.sync().start(0).toDestination());
+                  else
+                    reject(
+                      new Error(
+                        `Could not create track from track signed url ${track.trackSignedURL}`
+                      )
+                    );
+                });
+              });
             }
-        })();
-            
-    }, [locationState, demoId]);
 
-    return { demo, tracks, error, demoLoading, tracksLoading, recorder, setTracks };
-}
+            return { ...track, player };
+          })
+        );
+
+        setTracks(currentTracks);
+        setTracksLoading(false);
+        setError(null);
+      } catch (err) {
+        setDemo(null);
+        setTracks([]);
+        setError(err.message);
+        setDemoLoading(false);
+        setTracksLoading(false);
+      }
+    })();
+  }, [locationState, demoId]);
+
+  return {
+    demo,
+    tracks,
+    error,
+    demoLoading,
+    tracksLoading,
+    recorder,
+    setTracks,
+  };
+};

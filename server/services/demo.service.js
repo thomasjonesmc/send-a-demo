78b707f7afe1db6c9114e891417fbdcb5605c1fe
samstorment <sjstorment22@gmail.com>
2021-01-28 05:09:04 -0600

    Fixed server  function so it only queries db once by using populate function

--- server/services/demo.service.js
+++ server/services/demo.service.js
@@ -102,17 +102,19 @@ const addUserToDemo = async (demoId, userId) => {
         // add the user to the demo with the demoId if the user is not already in the array of contributors and not the creator
         { _id: demoId, creatorId: { $ne: userId }, contributors: { $ne: userId } },
         { $push: { contributors: userId } },
-    );
+        { new: true }
+    )
+    .populate({ path: "tracks", model: Track })
+    .populate({ path: "creatorId", model: User, select: '-__v -password'});
 
     if (!pushDemo) { error("Couldn't add user to demo"); }
 
-    // after we push the contributor to the demo, we fetch the updated demo
-    // I tried using the `new` flag on the `findOneAndUpdate` above, but I couldn't convert the creatorId to a user document in the same way that the `getDemos` function does
-    const [ updatedDemo ] = await getDemos({
-        _id: ObjectId(demoId)
-    });
+    const { creatorId: creator, ...demo } = pushDemo.toObject();
 
-    return updatedDemo;
+    return {
+        ...demo,
+        creator
+    }; 
 }
 
 // a helper function that gets demos, lets us pass in a custom mongo $match to filter
